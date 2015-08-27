5bac21dabd64b01e89b1b6a812b1e91b9a11c1e7
Tom <tom@tomcronin.org>
2015-08-27 18:57:43 +0100

    Added POST using requests that points at Slack webhook

--- splack.py
+++ splack.py
@@ -3,13 +3,18 @@
 # Not sure playerctl is the best way to go
 
 from gi.repository import Playerctl, GLib
+import requests
+
+webhookURL = #CHANGE
 
 player = Playerctl.Player()
 
 # On track change print out track info (for the moment)
 def on_track_change(player, e):
     track_info = '{artist} - {title}'.format(artist=player.get_artist(), title=player.get_title())
+	payload={"text": [track_info], "channel": "#general", "username": "SplackBot", "icon_emoji": ":headphones:"}
     print([track_info])
+	requests.post(webhookURL, params=payload)
 
 player.on('metadata', on_track_change)
 
