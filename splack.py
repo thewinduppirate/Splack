118b45612243f154028474258e0f6e9a61fed48e
Tom <tom@tomcronin.org>
2015-08-27 19:10:36 +0100

    Change params to payload to match Splack.

--- splack.py
+++ splack.py
@@ -12,9 +12,9 @@ player = Playerctl.Player()
 # On track change print out track info (for the moment)
 def on_track_change(player, e):
     track_info = '{artist} - {title}'.format(artist=player.get_artist(), title=player.get_title())
-	payload={"text": [track_info], "channel": "#general", "username": "SplackBot", "icon_emoji": ":headphones:"}
+	splackinfo={"text": [track_info], "channel": "#general", "username": "SplackBot", "icon_emoji": ":headphones:"}
     print([track_info])
-	requests.post(webhookURL, params=payload)
+	requests.post(webhookURL, payload=splackinfo)
 
 player.on('metadata', on_track_change)
 
