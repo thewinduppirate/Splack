358b32dcbc80632348ffb6a73f9759a813b34f18
Tom <tom@tomcronin.org>
2015-08-27 19:33:27 +0100

    Dropped requests, trying urllib instead.

--- splack.py
+++ splack.py
@@ -3,9 +3,19 @@
 # Not sure playerctl is the best way to go
 
 from gi.repository import Playerctl, GLib
-import requests
+import urllib
 
 webhookURL = #CHANGE
+
+params {}
+
+params["text"] = "This is posted to <#general> and comes from *SplackBot*."
+params["channel"] = "#general"
+params["username"] = "SplackBot"
+params["icon_emoji"] =":headphones:"
+
+payload = urllib.urlencode(params)
+post = urllib.urlopen(webhookURL, params)
 
 player = Playerctl.Player()
 
@@ -14,7 +24,7 @@ def on_track_change(player, e):
     track_info = '{artist} - {title}'.format(artist=player.get_artist(), title=player.get_title())
     splackinfo={"text": [track_info], "channel": "#general", "username": "SplackBot", "icon_emoji": ":headphones:"}
     print([track_info])
-    requests.post(webhookURL, payload=splackinfo)
+    print post.read()
 
 player.on('metadata', on_track_change)
 
