bc467f3dfd1472a88530e26befaa3ecf7addd98d
Tom <tom@tomcronin.org>
2015-08-28 09:36:58 +0100

    Need more coffee.

--- splack.py
+++ splack.py
@@ -16,7 +16,7 @@ params["username"] = "SplackBot"
 params["icon_emoji"] =":headphones:"
 
 payload = urllib.parse.urlencode(params)
-payload = data.encode("utf-8")
+payload = payload.encode("utf-8")
 request = urllib.request.Request(webhookURL)
 request.add_header("Content-Type","application/x-www-form-urlencoded;charset=utf-8")
 
